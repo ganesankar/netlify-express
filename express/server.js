190b37bc8d08983b6f0696d26bc21cd075506d81
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2020-07-02 18:05:16 +0530

    back

--- express/server.js
+++ express/server.js
@@ -4,19 +4,17 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const ejs = require('ejs');
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/../ui');
+app.use('/login', require('./login/'));
+
+app.use('/', require('./web/'));
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
